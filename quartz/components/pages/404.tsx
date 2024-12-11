5d1e9c0e569e40f6a5c009c9f6c6f2e093330661
unknown <rand.ludtke@gmail.com>
2024-12-11 00:11:40 -0600

    Quartz sync: Dec 11, 2024, 12:11 AM

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -1,18 +1,30 @@
-import { i18n } from "../../i18n"
-import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "../types"
+import { i18n } from "../../i18n";
+import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "../types";
 
 const NotFound: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
   // If baseUrl contains a pathname after the domain, use this as the home link
-  const url = new URL(`https://${cfg.baseUrl ?? "example.com"}`)
-  const baseDir = url.pathname
+  const url = new URL(`https://${cfg.baseUrl ?? "example.com"}`);
+  const baseDir = url.pathname;
 
   return (
-    <article class="popover-hint">
+    <article class="popover-hint" style={{ textAlign: "center", fontFamily: "monospace" }}>
       <h1>404</h1>
       <p>{i18n(cfg.locale).pages.error.notFound}</p>
+      <div>
+        <br />&nbsp;&nbsp;&nbsp;&nbsp;___________<br />
+        &nbsp;&nbsp;/&nbsp;Hey&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\\<br />
+        &nbsp;|&nbsp;Lost?&nbsp;\\<br />
+        &nbsp;|___________\\<br />
+        &nbsp;&nbsp;\____________\\<br />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\\<br />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\\
+        <br />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;😶<br />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/| |\<br />
+      </div>
       <a href={baseDir}>{i18n(cfg.locale).pages.error.home}</a>
     </article>
-  )
-}
+  );
+};
 
-export default (() => NotFound) satisfies QuartzComponentConstructor
+export default (() => NotFound) satisfies QuartzComponentConstructor;
--- quartz/plugins/emitters/404.tsx
+++ quartz/plugins/emitters/404.tsx
@@ -5,29 +5,11 @@ import { pageResources, renderPage } from "../../components/renderPage"
 import { FullPageLayout } from "../../cfg"
 import { FilePath, FullSlug } from "../../util/path"
 import { sharedPageComponents } from "../../../quartz.layout"
+import { NotFound } from "../../components"
 import { defaultProcessedContent } from "../vfile"
 import { write } from "./helpers"
 import { i18n } from "../../i18n"
 import DepGraph from "../../depgraph"
-
-const NotFound = () => {
-  return `
-    <div style="text-align: center; font-family: monospace;">
-      <pre>
-         _______________
-        |               |
-        |   404 Error   |
-        |_______________|
-            \\\|||///
-            ( O   O )
-         --ooo--( )--ooo---
-       Oh no! You seem lost.
-       Let's find a better path!
-      </pre>
-      <p><a href="/">Return to safety</a></p>
-    </div>
-  `;
-};
 
 export const NotFoundPage: QuartzEmitterPlugin = () => {
   const opts: FullPageLayout = {
@@ -36,33 +18,33 @@ export const NotFoundPage: QuartzEmitterPlugin = () => {
     beforeBody: [],
     left: [],
     right: [],
-  };
+  }
 
-  const { head: Head, pageBody, footer: Footer } = opts;
-  const Body = BodyConstructor();
+  const { head: Head, pageBody, footer: Footer } = opts
+  const Body = BodyConstructor()
 
   return {
     name: "404Page",
     getQuartzComponents() {
-      return [Head, Body, pageBody, Footer];
+      return [Head, Body, pageBody, Footer]
     },
     async getDependencyGraph(_ctx, _content, _resources) {
-      return new DepGraph<FilePath>();
+      return new DepGraph<FilePath>()
     },
     async emit(ctx, _content, resources): Promise<FilePath[]> {
-      const cfg = ctx.cfg.configuration;
-      const slug = "404" as FullSlug;
+      const cfg = ctx.cfg.configuration
+      const slug = "404" as FullSlug
 
-      const url = new URL(`https://${cfg.baseUrl ?? "example.com"}`);
-      const path = url.pathname as FullSlug;
-      const externalResources = pageResources(path, resources);
-      const notFound = i18n(cfg.locale).pages.error.title;
+      const url = new URL(`https://${cfg.baseUrl ?? "example.com"}`)
+      const path = url.pathname as FullSlug
+      const externalResources = pageResources(path, resources)
+      const notFound = i18n(cfg.locale).pages.error.title
       const [tree, vfile] = defaultProcessedContent({
         slug,
         text: notFound,
         description: notFound,
         frontmatter: { title: notFound, tags: [] },
-      });
+      })
       const componentData: QuartzComponentProps = {
         ctx,
         fileData: vfile.data,
@@ -71,7 +53,7 @@ export const NotFoundPage: QuartzEmitterPlugin = () => {
         children: [],
         tree,
         allFiles: [],
-      };
+      }
 
       return [
         await write({
@@ -80,7 +62,7 @@ export const NotFoundPage: QuartzEmitterPlugin = () => {
           slug,
           ext: ".html",
         }),
-      ];
+      ]
     },
-  };
-};
+  }
+}
